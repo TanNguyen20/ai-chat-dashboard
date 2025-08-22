8413f1167c48b36c818dd5f701df89ea52e01f66
Tan Nguyen <82394019+TanNguyen20@users.noreply.github.com>
2025-08-22 13:32:09 +0700

    create loading component

--- components/loading/index.tsx
+++ components/loading/index.tsx
@@ -1,5 +1,7 @@
 import "./index.css"
 
-<div class="loader">Loading
-  <span></span>
-</div>
+export default function Loading() {
+  return <div class="loader">Loading
+    <span></span>
+  </div>
+}
