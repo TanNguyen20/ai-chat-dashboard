a934646371682b736f2780e3c0a56808edcd978f
Tan Nguyen <82394019+TanNguyen20@users.noreply.github.com>
2025-10-09 13:33:28 +0700

    update: add page icon and page name

--- types/pageAccess.ts
+++ types/pageAccess.ts
@@ -5,12 +5,16 @@ export type RolePermissions = Record<RoleKey | string, CRUD>;
 export interface PageAccess {
   id: number;
   url: string;
+  pageName: string
+  pageIcon: string
   description: string;
   rolePermissions: RolePermissions;
 }
 
 export interface UpsertPageRequest {
   url: string;
+  pageName: string
+  pageIcon: string
   description: string;
   rolePermissions: RolePermissions;
 }
