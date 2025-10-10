c5e3183574d9b8a2d67345040a90edb813d429c0
Tan Nguyen <82394019+TanNguyen20@users.noreply.github.com>
2025-10-10 09:40:45 +0700

    update: try to hidden open in new tab

--- app/(dashboard)/stock/page.tsx
+++ app/(dashboard)/stock/page.tsx
@@ -3,7 +3,7 @@
 export default function StockAppEmbed() {
   return (
     <iframe
-      src="https://stock-ap.streamlit.app/?embed=true"
+      src="https://stock-ap.streamlit.app/?embedded=true"
       style={{ width: "100%", height: "100%", border: "0", overflow: "hidden" }}
       loading="lazy"
       referrerPolicy="no-referrer-when-downgrade"
